0a85bc185a81770d3f074c49133442174e2f0836
Lehi Bennett <merlinlcb@merlinlcb.com>
2024-01-21 19:29:59 -0700

    upload updates

--- js/customfunctions.js
+++ js/customfunctions.js
@@ -1,75 +1,124 @@
 function function1() {
   document.getElementById("nav1").classList.add("active");
-  document.getElementById("nav3").classList.remove("active");
   document.getElementById("nav2").classList.remove("active");
-  var x = document.getElementById("homecontainer");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "block";
-  }
-
-  var x = document.getElementById("aboutcontainer");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "none";
-  }
-
+  document.getElementById("nav3").classList.remove("active");
+  document.getElementById("nav4").classList.remove("active");
+  var w = document.getElementById("biocontainer");
   var x = document.getElementById("contactcontainer");
+  var y = document.getElementById("aboutcontainer");
+  var z = document.getElementById("homecontainer");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "block";
+  }
+  if (w.style.display === "block") {
+    w.style.display = "none";
+  } else {
+    w.style.display = "none";
+  }
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "none";
+  }
+
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "none";
   }
 }
 function function2() {
   document.getElementById("nav1").classList.remove("active");
-  document.getElementById("nav3").classList.remove("active");
   document.getElementById("nav2").classList.add("active");
-  var x = document.getElementById("aboutcontainer");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "block";
-  }
-
-  var x = document.getElementById("homecontainer");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "none";
-  }
-
+  document.getElementById("nav3").classList.remove("active");
+  document.getElementById("nav4").classList.remove("active");
+  var w = document.getElementById("biocontainer");
   var x = document.getElementById("contactcontainer");
+  var y = document.getElementById("aboutcontainer");
+  var z = document.getElementById("homecontainer");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "block";
+  }
+  if (w.style.display === "block") {
+    w.style.display = "none";
+  } else {
+    w.style.display = "none";
+  }
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "none";
+  }
+
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "none";
   }
 }
 function function3() {
   document.getElementById("nav1").classList.remove("active");
-  document.getElementById("nav3").classList.add("active");
   document.getElementById("nav2").classList.remove("active");
+  document.getElementById("nav3").classList.add("active");
+  document.getElementById("nav4").classList.remove("active");
+  var w = document.getElementById("biocontainer");
   var x = document.getElementById("contactcontainer");
+  var y = document.getElementById("aboutcontainer");
+  var z = document.getElementById("homecontainer");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "block";
   }
+  if (w.style.display === "block") {
+    w.style.display = "none";
+  } else {
+    w.style.display = "none";
+  }
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "none";
+  }
 
-  var x = document.getElementById("aboutcontainer");
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "none";
+  }
+}
+function function4() {
+  document.getElementById("nav1").classList.remove("active");
+  document.getElementById("nav3").classList.remove("active");
+  document.getElementById("nav2").classList.remove("active");
+  document.getElementById("nav4").classList.add("active");
+  var w = document.getElementById("biocontainer");
+  var x = document.getElementById("contactcontainer");
+  var y = document.getElementById("aboutcontainer");
+  var z = document.getElementById("homecontainer");
+  if (w.style.display === "none") {
+    w.style.display = "block";
+  } else {
+    w.style.display = "block";
+  }
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "none";
   }
-
-  var x = document.getElementById("homecontainer");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  if (y.style.display === "block") {
+    y.style.display = "none";
   } else {
-    x.style.display = "none";
+    y.style.display = "none";
+  }
+
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "none";
   }
 }
